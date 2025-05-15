6b27ed98915a9a9c8a84744070efbedc35bceb47
FD5GD <s20bar.gd@gmail.com>
2025-05-16 00:18:14 +0200

    Shared Model - added DBScoresByPreset for getting all scores at once

---
+++ ProjectKBShared/Model/DBScoresByPreset.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectKBShared.Model
+{
+    public class DBScoresByPreset
+    {
+        public byte preset { get; set; }
+        public List<DBScore> scores { get; set; }
+    }
+}
